3425697f38622cf708d889e57614fe11c763f596
Ole Holst Andersen <oha@nota.nu>
2012-09-27 22:09:05 +0200

    Now loads domain list file

--- src/nota/oxygen/common/links/MarkupLinksOperation.java
+++ src/nota/oxygen/common/links/MarkupLinksOperation.java
@@ -1,5 +1,16 @@
 package nota.oxygen.common.links;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.ls.DOMImplementationLS;
+import org.w3c.dom.ls.LSInput;
+import org.w3c.dom.ls.LSParser;
+
 import ro.sync.ecss.extensions.api.ArgumentDescriptor;
 import ro.sync.ecss.extensions.api.ArgumentsMap;
 import ro.sync.ecss.extensions.api.AuthorOperationException;
@@ -15,15 +26,65 @@ public class MarkupLinksOperation extends BaseAuthorOperation {
 
 	@Override
 	protected void doOperation() throws AuthorOperationException {
-		// TODO Auto-generated method stub
-
+		
 	}
 	
-	private String[] getDomainList()
+	String[] domainList;
+	
+	private String[] getDomainList() throws AuthorOperationException
 	{
-		
-		return null;
+		if (domainList == null)
+		{
+			Element root = getDomainListDocument().getDocumentElement();
+			if (root==null) 
+			{
+				domainList String[0];
+			}
+			else
+			{
+				NodeList domains = getDomainListDocument().getElementsByTagName("domain");
+				domainList = new String[domains.getLength()];
+				for (int i=0; i<domainList.length; i++)
+				{
+					NodeList l = ((Element)domains.item(i)).getElementsByTagName("name");
+					if (l.getLength()>0)
+					{
+						domainList[i] = "";
+					}
+					else
+					{
+						domainList[i] = l.item(0).getTextContent();
+					}
+				}
+			}
+		}
+		return domainList;
 	}
+	
+	private Document domainListDocument;
+	
+	protected void loadDomainListDocument() throws AuthorOperationException {
+		InputStream is;
+		try {
+			is = new  FileInputStream(domainListFile);
+		} catch (FileNotFoundException e) {
+			throw new AuthorOperationException("Could not find domain list file "+domainListFile);
+		}
+		DOMImplementationLS impl = getDOMImplementation();
+		LSParser builder = impl.createLSParser(DOMImplementationLS.MODE_SYNCHRONOUS, null);
+		LSInput input = impl.createLSInput();
+		input.setByteStream(is);
+		input.setEncoding("UTF-8");
+		domainListDocument =  builder.parse(input);
+	}
+	
+	protected Document getDomainListDocument() throws AuthorOperationException {
+		if (domainListDocument==null) {//Lazy loading of settings document
+			loadDomainListDocument();
+		} 
+		return domainListDocument;
+	}
+
 
 	@Override
 	protected void parseArguments(ArgumentsMap args)
@@ -33,6 +94,7 @@ public class MarkupLinksOperation extends BaseAuthorOperation {
 		externalAttributeName = (String)args.getArgumentValue(ARG_EXTERNAL_ATTRIBUTE_NAME);
 		if (externalAttributeName==null) externalAttributeName = "";
 		externalAttributeName = externalAttributeName.trim();
+		domainListFile = (String)args.getArgumentValue(ARG_DOMAIN_LIST_FILE);
 	}
 	
 	private static String ARG_LINK_FRAGMENT = "link fragment";
@@ -50,7 +112,8 @@ public class MarkupLinksOperation extends BaseAuthorOperation {
 		return new ArgumentDescriptor[] {
 				new ArgumentDescriptor(ARG_LINK_FRAGMENT, ArgumentDescriptor.TYPE_FRAGMENT, "link xml fragment"),
 				new ArgumentDescriptor(ARG_REF_ATTRIBUTE_NAME, ArgumentDescriptor.TYPE_STRING, "name of reference attribute (href)"),
-				new ArgumentDescriptor(ARG_EXTERNAL_ATTRIBUTE_NAME, ArgumentDescriptor.TYPE_STRING, "name of external attribute (external)")
+				new ArgumentDescriptor(ARG_EXTERNAL_ATTRIBUTE_NAME, ArgumentDescriptor.TYPE_STRING, "name of external attribute (external)"),
+				new ArgumentDescriptor(ARG_DOMAIN_LIST_FILE, ArgumentDescriptor.TYPE_STRING, "name of external attribute (external)")
 		};
 	}
 
