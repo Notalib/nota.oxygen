0134b094c312c8f1b9553949b8a9243f13c7a3aa
Yakup Batak <ybk@nota.nu>
2015-06-24 15:25:54 +0200

    fixed meta data error

--- addins/src/nota/oxygen/epub/SplitHandler.java
+++ addins/src/nota/oxygen/epub/SplitHandler.java
@@ -2,10 +2,9 @@ package nota.oxygen.epub;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
-
 import org.xml.sax.*;
 import org.xml.sax.helpers.DefaultHandler;
 
@@ -18,7 +17,7 @@ public class SplitHandler extends DefaultHandler {
 	
 	public SplitHandler() {
 		htmlAttributes = new HashMap<String, String>();
-		metaNodes = new TreeMap<String, String>();
+		metaNodes = new LinkedHashMap <String, String>();
 		cssLinks = new ArrayList<String>();
 	}
 	
